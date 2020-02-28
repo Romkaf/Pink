7334c826ad93cd71f407feeaec14a02a9ffaa7e6
Roman <romkaft@mail.ru>
2020-02-28 15:56:15 +0300

    Desktop first. Header for desktop and tablet

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+ 
+// sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/style.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({stream:true}));
+    
+});
+
+gulp.task('browser-sync', function() { 
+  browserSync({ 
+      server: { 
+          baseDir: '.' 
+      },
+      notify: false 
+  });
+});
+
+gulp.task('code', function() {
+  return gulp.src('./*.html')
+  .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.parallel('sass'));
+  gulp.watch('./*.html', gulp.parallel('code')); 
+});
+
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
