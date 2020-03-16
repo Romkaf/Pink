b0b0f3b047e670e77463f14817add7af9add89bb
Roman <romkaft@mail.ru>
2020-03-16 10:36:06 +0300

    Add section contacts and footer. Made refactoring for slider.js

--- js/slider.js
+++ js/slider.js
@@ -11,10 +11,33 @@ function currentReview(n) {
   showReviews(n);
 }
 
+
 function showReviews(n) {
   var i;
   var reviews = document.getElementsByClassName("reviews__block");
   var toggles = document.getElementsByClassName("reviews__toggle");
+// Следит за изменением ширины экрана на 660 px и при большем значении даёт нужному отзыву display:block, а при меньше 660px присваивает display:flex. 
+  const tabletMediaQuery = window.matchMedia('(min-width: 660px)');
+
+  tabletMediaQuery.addListener(function(evt) {
+    // console.log(evt);
+    const condition = evt.matches;
+
+    if(condition){
+      reviews[reviewIndex - 1].style.display = "block";
+    } else {
+        reviews[reviewIndex - 1].style.display = "flex";
+      }
+  });
+
+// Присваивает block текущему отзыву при вьюпорте > 660px, иначе flex.
+  const handleResize = function() {
+    if(tabletMediaQuery.matches){
+      reviews[reviewIndex - 1].style.display = "block";
+    } else {
+        reviews[reviewIndex - 1].style.display = "flex";
+      }
+  };
 
   if (n > reviews.length) {
     reviewIndex = 1; 
@@ -29,57 +52,48 @@ function showReviews(n) {
   }
 
   for (i = 0; i < toggles.length; i++) {
-    toggles[i].className = toggles[i].className.replace("active", "");
-    // toggles[i].classList.remove('active');
+    toggles[i].classList.remove('active');
   }
 
-  if(window.matchMedia('(max-width: 659px)').matches){
-    reviews[reviewIndex - 1].style.display = "flex";
-    toggles[reviewIndex - 1].className += " active";
-  } else {
-      reviews[reviewIndex - 1].style.display = "block";
-      toggles[reviewIndex - 1].className += " active";
-    } 
-  window.onresize = function() {
-      if(window.matchMedia('(min-width: 660px)').matches){
-        reviews[reviewIndex - 1].style.display = "block";
-      } else {
-          reviews[reviewIndex - 1].style.display = "flex";
-        }
-    };
+  toggles[reviewIndex - 1].className += " active";
+// Ф-ция выполняется каждый раз при нажатии на кнопку или загрузки страницы
+  handleResize(); 
 }
 
 var prev = document.querySelector('.reviews__prev');
 var next = document.querySelector('.reviews__next');
-var review = document.querySelectorAll('.reviews__block');
-var buttonToggle = document.querySelectorAll('.reviews__toggle');
+var reviews = document.querySelectorAll('.reviews__block');
+var buttonToggles = document.querySelectorAll('.reviews__toggle');
+// Присваение блокам отзывов анмации 'translate-left' при нжатии на prev и 'translate-right' при нажатии на next.
+const handleArrowClick = function(direction) {
+  for (let i = 0; i < reviews.length; i++) {
+    reviews[i].classList.remove('translate-left');
+    reviews[i].classList.remove('translate-right');
 
-prev.addEventListener('click', function() {
-  for (let i = 0; i < review.length; i++) {
-    review[i].classList.remove('translate-right');
-    review[i].classList.add('translate-left'); 
+   if (direction === 'prev') {
+    reviews[i].classList.add('translate-left'); 
     setTimeout (function() {
-      review[i].classList.remove('translate-left');;
+      reviews[i].classList.remove('translate-left');
     }, 600);
-  }
-});
-
-next.addEventListener('click', function() {
-  for (let i = 0; i < review.length; i++) {
-    review[i].classList.remove('translate-left');
-    review[i].classList.add('translate-right');
+   } else {
+    reviews[i].classList.add('translate-right');
     setTimeout (function() {
-      review[i].classList.remove('translate-right');;
+      reviews[i].classList.remove('translate-right');
     }, 600);
+   }
   }
-});
+}
 
+prev.addEventListener('click', function() { handleArrowClick('prev') });
+next.addEventListener('click', function() { handleArrowClick() });
+
+//Присваение блокам отзывов  класса с анимацией fade и удалением этого класса через 600мс
 for (let j = 0; j < 3; j++) {
-  buttonToggle[j].addEventListener('click', function() {
-    for (let i = 0; i < review.length; i++) {
-      review[i].classList.add('fade-animation');
+  buttonToggles[j].addEventListener('click', function() {
+    for (let i = 0; i < reviews.length; i++) {
+      reviews[i].classList.add('fade-animation');
       setTimeout (function() {
-        review[i].classList.remove('fade-animation');;
+        reviews[i].classList.remove('fade-animation');;
       }, 600);
     }
   }); 
